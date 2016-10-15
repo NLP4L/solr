1dd254b89b5b4681d241a007db250510cd885d70
koji <koji.sekiguchi@rondhuit.com>
2016-10-15 14:24:09 +0900

    add FieldFeatureStoredValueExtractor

--- src/test/java/org/nlp4l/solr/ltr/LinearWeightQueryTest.java
+++ src/test/java/org/nlp4l/solr/ltr/LinearWeightQueryTest.java
@@ -35,12 +35,14 @@ public class LinearWeightQueryTest extends LuceneTestCase {
   @Test
   public void testToStringBeforeInit() throws Exception {
     LinearWeightQuery linearWeightQuery = new LinearWeightQuery(buildFeaturesSpec(
-            getTF("TF in title", "title"), getTF("TF in body", "body"), getIDF("IDF in title", "title")),
-            buildWeights(3.0F, 4.0F, 1.5F));
+            getTF("TF in title", "title"), getTF("TF in body", "body"), getIDF("IDF in title", "title"), getTFIDF("TFIDF in body", "body")),
+            buildWeights(3.0F, 4.0F, 1.5F, 2.5F));
     assertEquals("org.nlp4l.solr.ltr.LinearWeightQuery featuresSpec=[" +
                     "org.nlp4l.solr.ltr.FieldFeatureTFExtractorFactory[]," +
                     "org.nlp4l.solr.ltr.FieldFeatureTFExtractorFactory[]," +
-                    "org.nlp4l.solr.ltr.FieldFeatureIDFExtractorFactory[]], weights=[3.0,4.0,1.5]",
+                    "org.nlp4l.solr.ltr.FieldFeatureIDFExtractorFactory[]," +
+                    "org.nlp4l.solr.ltr.FieldFeatureTFIDFExtractorFactory[]]," +
+                    " weights=[3.0,4.0,1.5,2.5]",
             linearWeightQuery.toString());
   }
 
@@ -74,12 +76,14 @@ public class LinearWeightQueryTest extends LuceneTestCase {
     Document doc = new Document();
     doc.add(new StringField("title", "foo", Field.Store.YES));
     doc.add(new StringField("body", "foo", Field.Store.YES));
+    doc.add(new StringField("len", "12", Field.Store.YES));
     w.addDocument(doc);
     w.getReader().close();
 
     doc = new Document();
     doc.add(new StringField("title", "foo", Field.Store.YES));
     doc.add(new StringField("body", "bar", Field.Store.YES));
+    doc.add(new StringField("len", "22", Field.Store.YES));
     w.addDocument(doc);
     w.getReader().close();
 
@@ -90,22 +94,23 @@ public class LinearWeightQueryTest extends LuceneTestCase {
             getTF("TF in title", "title", context, new Term("title", "foo")),
             getTF("TF in body", "body", context, new Term("body", "bar")),
             getIDF("IDF in title", "title", context, new Term("title", "foo")),
-            getTFIDF("TFIDF in body", "body", context, new Term("body", "bar"))),
-            buildWeights(3.0F, 4.0F, 1.5F, 2.5F));
+            getTFIDF("TFIDF in body", "body", context, new Term("body", "bar")),
+            getSV("length in body", "len", context, new Term("title", "foo"), new Term("body", "foo"))),
+            buildWeights(3.0F, 4.0F, 1.5F, 2.5F, 6.0F));
 
     IndexSearcher searcher = new IndexSearcher(reader);
     TopDocs topDocs = searcher.search(linearWeightQuery, 10);
     assertEquals(2, topDocs.totalHits);
 
     // title: foo, body:bar
-    float expectedScore = 3 * 1 + 4 * 1 + 1.5F * (float)Math.log(3.0 / 2.0) + 2.5F * 1 * (float)Math.log(3.0 / 1.0);
-    assertEquals(expectedScore, topDocs.scoreDocs[0].score, 0.0005);
     assertEquals("bar", searcher.doc(topDocs.scoreDocs[0].doc).get("body"));
+    float expectedScore = 3 * 1 + 4 * 1 + 1.5F * (float)Math.log(3.0 / 2.0) + 2.5F * 1 * (float)Math.log(3.0 / 1.0) + 22 * 6.0F;
+    assertEquals(expectedScore, topDocs.scoreDocs[0].score, 0.0005);
 
     // title: foo, body:foo
-    expectedScore = 3 * 1 + 4 * 0 + 1.5F * (float)Math.log(3.0 / 2.0) + 2.5F * 0 * (float)Math.log(3.0 / 1.0);
-    assertEquals(expectedScore, topDocs.scoreDocs[1].score, 0.0005);
     assertEquals("foo", searcher.doc(topDocs.scoreDocs[1].doc).get("body"));
+    expectedScore = 3 * 1 + 4 * 0 + 1.5F * (float)Math.log(3.0 / 2.0) + 2.5F * 0 * (float)Math.log(3.0 / 1.0) + 12 * 6.0F;
+    assertEquals(expectedScore, topDocs.scoreDocs[1].score, 0.0005);
 
     IOUtils.close(reader, w, dir);
   }
@@ -143,6 +148,19 @@ public class LinearWeightQueryTest extends LuceneTestCase {
 
   private FieldFeatureExtractorFactory getTFIDF(String featureName, String fieldName, IndexReaderContext context, Term... terms){
     FieldFeatureExtractorFactory factory = new FieldFeatureTFIDFExtractorFactory(featureName, fieldName);
+    if(context != null){
+      factory.init(context, terms);
+    }
+    return factory;
+  }
+
+  private FieldFeatureExtractorFactory getSV(String featureName, String fieldName){
+    FieldFeatureExtractorFactory factory = new FieldFeatureStoredValueExtractorFactory(featureName, fieldName);
+    return factory;
+  }
+
+  private FieldFeatureExtractorFactory getSV(String featureName, String fieldName, IndexReaderContext context, Term... terms){
+    FieldFeatureExtractorFactory factory = new FieldFeatureStoredValueExtractorFactory(featureName, fieldName);
     if(context != null){
       factory.init(context, terms);
     }
